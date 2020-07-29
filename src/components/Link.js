73ea9f3a10f3e0c68beb68e9326aa34572670e2e
azerbelhedi <azerbelhedide@gmail.com>
2020-07-29 11:15:18 +0200

    fetch votes

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
 
-export default function Link({link}){
-    return(
+export default function Link({ link, index }) {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  const _voteForLink = () => {};
+
+  return (
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div className="ml1 gray f11" onClick={() => _voteForLink()}>
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
         <div>
-            <div>
-                {link.description} {link.url} {link.createdAt}
-            </div>
+          {link.description} ({link.url})
         </div>
-    )
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{" "}
+          {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,21 +6,23 @@ import { Query } from "react-apollo";
 export default function LinkList() {
   return (
     <Query query={FEED_QUERY}>
-     {
-         ({ loading , error, data}) => {
-             if(loading){return(<div>Fetching</div>)}
-             if(error){return(<div>error</div>)}
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <div>Fetching</div>;
+        }
+        if (error) {
+          return <div>error</div>;
+        }
 
-             const linksToRender = data.feed.links
-             return(
-                 <div>
-                     {
-                        linksToRender.map((link) => <Link key={link.id} link={link} />)
-                     }
-                 </div>
-             )
-         }
-     }
+        const linksToRender = data.feed.links;
+        return (
+          <div>
+            {linksToRender.map((link, index) => (
+              <Link key={link.id} link={link} index={index} />
+            ))}
+          </div>
+        );
+      }}
     </Query>
   );
 }
@@ -33,6 +35,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
